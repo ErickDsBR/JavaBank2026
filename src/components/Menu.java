93a528f7dcdd5a99b44aed879babd01e7896b14e
Erick Brasil <erick.brasil2102@gmail.com>
2026-08-25 09:59:32 -0300

    Add files via upload

--- src/components/Menu.java
+++ src/components/Menu.java
@@ -1,31 +1,27 @@
 package components;
 
 import java.util.Scanner;
-import modules.Usermodules;
-
 
 public class Menu {
-    Usermodules usermodules = new Usermodules();
     public static void ShowMenu(){
         Scanner sc = new Scanner(System.in);
 
 
-        opcao = 0;
 
-        while(true) {
+        int opcao = 0;
+
+        do {
             System.out.println("===MENU===");
             System.out.println("Chose An Option");
             System.out.println("1 - LOGIN");
             System.out.println("2 - Register");
             System.out.println("3- sair");
-            sc.hasNextLine();
-        }
-       switch (opcao) {
-           case 1:
-               System.out.println("password");
-               int userPassword = interger.parseInt(sc.hasNextLine());
+            opcao = Integer.parseInt(sc.nextLine());
 
         }
+        while (opcao != 3);
+
+
     }
 
 
