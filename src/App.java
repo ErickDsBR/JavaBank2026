6471b8a107d5cff0d302633c17cc2c8ca966396b
Erick Brasil <erick.brasil2102@gmail.com>
2026-08-25 10:02:42 -0300

    Delete src directory

--- src/App.java
+++
@@ -1,8 +0,0 @@
-import components.Menu;
-
-public class App {
-    public static void main(String args[]){
-        Menu.ShowMenu();
-
-    }
-}
--- src/modules/Usermodules.java
+++
@@ -1,9 +0,0 @@
-package modules;
-
-public class Usermodules {
-
-    public class void UserData {
-        int password = 12345;
-
-    }
-}
