3f3363ef053863de2db46316a159ab4ad074d7ff
ErickDsBR <erick.brasil2102@gmail.com>
2026-08-25 13:27:46 -0300

    feat: add UserData model class with account number, name and balance

---
+++ src/modules/UserData.java
@@ -0,0 +1,13 @@
+ package modules;
+
+public class UserData {
+    public String accountNumber;
+    public String name;
+    public double balance;
+
+    public UserData(String accountNumber, String name, double balance) {
+        this.accountNumber = accountNumber;
+        this.name = name;
+        this.balance = balance;
+    }   
+}
