56238fd50b91f988ef91590fd8dfecc47cb03c13
ErickDsBR <erick.brasil2102@gmail.com>
2026-09-01 12:58:53 -0300

    refactor: add login verification method to Session

--- src/modules/UserData/Session.java
+++ src/modules/UserData/Session.java
@@ -2,4 +2,8 @@ package modules.UserData;
 
 public class Session {
     public static UserData currentUser; 
+
+    public static boolean isUserLoggedIn() {
+        return currentUser != null;
+    }
 }
