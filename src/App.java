8655e5e696841300000b3f7daa734e966678a2a3
ErickDsBR <erick.brasil2102@gmail.com>
2026-09-01 12:58:04 -0300

    refactor: remove Menu.java and update App to use ClientMenu

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
-import components.Menu;
-
 public class App {
+
     public static void main(String args[]){
-        Menu.ShowMenu();
+        ClientMenu clientMenu = new ClientMenu();
+        clientMenu.ShowClientMenu();
 
     }
 }
